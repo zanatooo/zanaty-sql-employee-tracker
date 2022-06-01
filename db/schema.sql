eee2ae367482178bdf7be9338a5ae4103a3f8efa
zanatooo <m8elzanaty@gmail.com>
2022-05-31 18:38:40 -0700

    adding the seeds.sql file

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS employee_tracker;
+CREATE DATABASE employee_tracker;
+
+USE employee_tracker;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  department_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary decimal(10,2),
+  deparment_id int references department(id)
+
+);
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id int references role(id),
+  manager_id int references employee(id)
+  
+);
